84c13f7c83173f8a41985e6954ec385eb1ab5a44
Vinay Kumar <vinaydagar514@gmail.com>
2025-08-05 21:28:10 +0530

    add gradients circle

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,33 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import FloatingShapes from "@/components/floating-shapes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Pixel",
+  description: "AI Image Editor",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          {/* <Headers /> */}
+
+          <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+            <FloatingShapes />
+            <Toaster richColors />
+            {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/floating-shapes.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const FloatingShapes = () => {
+  const shapes = [
+    {
+      id: 1,
+      size: "w-72 h-72",
+      position: "top-20 left-10",
+      gradient: "from-blue-500 to-purple-600",
+    },
+    {
+      id: 2,
+      size: "w-96 h-96",
+      position: "top-1/3 right-10",
+      gradient: "from-cyan-400 to-blue-500",
+    },
+    {
+      id: 3,
+      size: "w-64 h-64",
+      position: "bottom-20 left-1/4",
+      gradient: "from-purple-500 to-pink-500",
+    },
+    {
+      id: 4,
+      size: "w-80 h-80",
+      position: "bottom-1/3 right-1/4",
+      gradient: "from-green-400 to-cyan-500",
+    },
+  ];
+  return (
+    <div className="fixed inset-0 overflow-hidden pointer-events-none">
+      {shapes.map((shape) => {
+        return (
+          <div
+            className={`absolute ${shape.size} ${shape.position} bg-gradient-to-r ${shape.gradient} rounded-full blur-3xl opacity-20 animate-pulse`}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default FloatingShapes;
