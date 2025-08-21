a5c47c7d873c54301de1124285413ede696f090b
Vinay Kumar <vinaydagar514@gmail.com>
2025-08-21 23:58:23 +0530

    Protected Route

--- components/header.jsx
+++ components/header.jsx
@@ -8,6 +8,7 @@ import { Button } from "./ui/button";
 import { useStoreUser } from "@/hooks/use-store-user";
 import { BarLoader } from "react-spinners";
 import { Authenticated, Unauthenticated } from "convex/react";
+import { LayoutDashboard } from "lucide-react";
 
 const Header = () => {
   const path = usePathname();
@@ -63,6 +64,12 @@ const Header = () => {
             </SignUpButton>
           </Unauthenticated>
           <Authenticated>
+            <Link href="/dashboard">
+              <Button variant="glass">
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:flex">Dashboard</span>
+              </Button>
+            </Link>
             <UserButton
               appearance={{
                 elements: {
--- middleware.js
+++ middleware.js
@@ -1,6 +1,19 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createdRouteMatcher(["/dashboard(.*)", "/editor(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
